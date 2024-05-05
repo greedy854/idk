12080a983aebec7f10928eef5e4eeded2211f1e7
sem <34904@ma-web.nl>
2024-05-05 23:50:26 +0200

    Skills page and Projects page

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { HashRouter as Router, Route } from 'react-router-dom';
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon2 from '../assets/img/tiktok.png';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 
@@ -52,7 +52,7 @@ export const NavBar = () => {
                 <a href="#"><img src={navIcon3} alt="Icon 3" /></a>
               </div>
               <HashLink smooth to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>Let’s Talk!</span></button>
               </HashLink>
             </span>
             </Navbar.Collapse>
