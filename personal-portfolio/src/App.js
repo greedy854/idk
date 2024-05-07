e2e6b1d54a1d44bd8330960c3e661ec4db488fc6
sem <34904@ma-web.nl>
2024-05-07 06:29:46 +0200

    downloads en contacts and newsletter almost done!

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -5,6 +5,7 @@ import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css'; // corrected typo here
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects/>
+      <Contact/>
     </div>
   );
 }
